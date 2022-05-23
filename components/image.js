a46f572e17c81e6d31b4d8295f7c4a30668a6bb8
Yosiet <yosietserga@gmail.com>
2022-05-23 11:06:59 -0400

    Update image.js

--- components/image.js
+++ components/image.js
@@ -1,6 +1,10 @@
+import React from "react";
 import Image from "next/image";
+import dynamic from "next/dynamic";
+import { load } from "dotenv";
 
 //walk through all images into public folder
+/*
 function importAll(r) {
   let images = {};
   r.keys().map((item) => {
@@ -18,8 +22,32 @@ cacheImages = importAll(
     /\.(png|jpe?g|svg)$/
   )
 );
+*/
 
 export default function Img({ s, a, c, w, h, l }) {
+  const [imageSrc, setImageSrc] = React.useState();
+  const base_path = process.env.NEXT_PUBLIC_IMAGE_PATH;
+  
+  dynamic(() => {
+    import(base_path + s).then((img) => {
+      console.log({img});
+      setImageSrc(img)
+    });
+    
+  });
+  /*
+  const loadImage = React.useCallback(
+    (img) => {
+      dynamic(() => {
+        import(base_path + img).then((img) => setImageSrc(img));
+      });
+    },
+    [setImageSrc, base_path]
+  );
+  React.useEffect(()=>{
+    loadImage(s);
+  }, [loadImage, s]);
+  */
   /*
   let subfolders = s.split('/');
   let basedir = process.env.NEXT_PUBLIC_IMAGE_PATH;
@@ -30,7 +58,10 @@ export default function Img({ s, a, c, w, h, l }) {
   }
   */
 
+  console.log({imageSrc});
+
   //not chached? let's get it
+  /*
   if (!cacheImages[s]) {
     let images = importAll(
       require.context(
@@ -42,10 +73,12 @@ export default function Img({ s, a, c, w, h, l }) {
 
     cacheImages = { ...cacheImages, images };
   }
+  */
 
   c = c ?? "";
   return (
     <>
+      {/*
       {!!cacheImages[s] && (
         <img
           className={"ui-image" + (c ? " " : "") + c}
@@ -55,30 +88,29 @@ export default function Img({ s, a, c, w, h, l }) {
           width={w ?? ""}
         />
       )}
-      {/*
 
-      {!!cacheImages[s] && (
-        <div
-          style={{
-            width: `${w ?? "100%"}`,
-            height: `${h ?? "100%"}`,
-            position: "relative",
-          }}
-        >
-          <Image
-            className={"ui-image" + (c ? " " : "") + c}
-            src={cacheImages[s]}
-            layout={l ?? "fill"}
-            alt={a ?? ""}
-            width="100%"
-            height="100%"
-            objectFit="contain"
-          />
-        </div>
-      )}
       */}
+      {imageSrc && (
+      <div
+        style={{
+          width: `${w ?? "100%"}`,
+          height: `${h ?? "100%"}`,
+          position: "relative",
+        }}
+      >
+        <Image
+          className={"ui-image" + (c ? " " : "") + c}
+          src={imageSrc}
+          layout={l ?? "fill"}
+          alt={a ?? ""}
+          width="100%"
+          height="100%"
+          objectFit="contain"
+        />
+      </div>
+      )}
 
-      {!cacheImages[s] && <small>Image not found: {s}</small>}
+      {/*{!cacheImages[s] && <small>Image not found: {s}</small>}*/}
     </>
   );
 }
